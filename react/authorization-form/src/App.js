8ddf4493b8c785f1585213c8e0d04bf1494e8176
Vladyslav <56609491+yarovit-developer@users.noreply.github.com>
2024-10-02 22:51:29 +0300

    Add files via upload

---
+++ react/authorization-form/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Contact from './Contact'; // Импорт компонента Contact
+
+function App() {
+  return <Contact />;
+}
+
+export default App;
---
+++ react/authorization-form/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Подключение стилей
+
+import App from './App'; // Импорт основного компонента
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
